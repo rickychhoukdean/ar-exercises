2a59900c6b71a79e2b0889f8c429d457849aac9a
Ricky <chhoukdean184@msn.com>
2019-09-16 18:55:28 +0000

    Use active record to test out read and update

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,14 @@
-require_relative '../setup'
-require_relative './exercise_1'
+require_relative "../setup"
+require_relative "./exercise_1"
 
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Old Navy"
+@store1.save
+
+pp @store1
 # Your code goes here ...
