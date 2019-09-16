645cb8e8f2a17f7e2ea096b4ffe2d89008535597
Ricky <chhoukdean184@msn.com>
2019-09-16 19:43:58 +0000

    Use active record with where command

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,23 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
