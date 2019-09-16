5928f3cd3f0d4feac5ac306d893e2969abb5f1b3
Ricky <chhoukdean184@msn.com>
2019-09-16 19:47:58 +0000

    Use activerecord calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,13 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+pp Store.sum(:annual_revenue)
+pp Store.average(:annual_revenue)
+pp Store.where("annual_revenue > ?", 1000000).count
